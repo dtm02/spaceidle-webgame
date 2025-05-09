0e64787cf6cbc20d5c4a7052a15586c82eb3b5b5
dtm02 <123612525+dtm02@users.noreply.github.com>
2025-05-08 21:26:46 -0500

    add support for serving JS files in 'client/src' to client

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,24 +1,47 @@
 // Http2 is likely a better option here, but the class names all start with Http2, so not quite as readable.
 import { Server, IncomingMessage, ServerResponse, createServer, type OutgoingHttpHeaders, type OutgoingHttpHeader } from 'http'
 import htmlIndex from './htmlIndex.ts'
+import { ACCESS_PORT, HOST_ADDRESS } from '../../shared/globals.ts'
+import fs from 'fs'
+import { resolve } from 'path'
 
-const HOST = "127.0.0.1" // must be IP, maybe domains work? idk
-const PORT = 3333
+function getFile(atUrl) {
+    const FILE_PATH = './client' + atUrl
+    console.log(resolve(FILE_PATH))
+    const descriptor = fs.readFileSync('./client' + atUrl)
+    return descriptor.toString()
+}
 
 const server: Server<typeof IncomingMessage, typeof ServerResponse> 
     = createServer((request: IncomingMessage, response: ServerResponse<IncomingMessage>) => {
         // https://mimesniff.spec.whatwg.org/#mime-type-groups content-type should be a valid mime-type
-        const outgoingHeader: OutgoingHttpHeaders = {
-            "content-type": 'text/html'
-        }
-        response.writeHead(200, outgoingHeader)
-        response.write(htmlIndex(), (err) => {
-            if (!err) {
-                console.log("Successfully wrote to stream")
-            } else {
-                console.error("Error in writing to stream")
+        if (request.url === '/') {
+            const outgoingHeader: OutgoingHttpHeaders = {
+                "content-type": 'text/html'
             }
-        })
-        response.end()
+            response.writeHead(200, outgoingHeader)
+            response.write(htmlIndex(), (err) => {
+                if (!err) {
+                    console.log("Successfully wrote index.html to stream")
+                } else {
+                    console.error("Error in writing index.html to stream")
+                }
+            })
+            response.end()
+        } else if (request.url.startsWith('/src/')) {
+            //console.log(getFile(request.url))
+            const outgoingHeader: OutgoingHttpHeaders = {
+                "content-type": "text/javascript"
+            }
+            response.writeHead(200, outgoingHeader)
+            response.write(getFile(request.url), (err) => {
+                if (!err) {
+                    console.log("Successfully sent JS!")
+                } else {
+                    console.log("Could not send js :(")
+                }
+            })
+            response.end()
+        }
     })
-server.listen(PORT, HOST) // HOST argument is not necessary, defaults to localhost in that case
+server.listen(ACCESS_PORT, HOST_ADDRESS) // HOST argument is not necessary, defaults to localhost in that case
---
+++ shared/globals.ts
@@ -0,0 +1,3 @@
+export const HOST_ADDRESS = '127.0.0.1'
+export const SOCKET_PORT = 3334
+export const ACCESS_PORT = 3333
